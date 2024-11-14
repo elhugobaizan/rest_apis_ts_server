0dac30ffe692e20e45fa49b07ee57828578e4393
Hugo Baizan <hbaizan@gmail.com>
2024-11-14 12:30:24 -0300

    remove

--- dist/config/swagger.d.ts
+++
@@ -1,5 +0,0 @@
-import { SwaggerUiOptions } from "swagger-ui-express";
-declare const swaggerSpec: object;
-declare const swaggerUIOptions: SwaggerUiOptions;
-export default swaggerSpec;
-export { swaggerUIOptions };
--- dist/models/Product.model.d.ts
+++
@@ -1,8 +0,0 @@
-import Sequelize, { Model } from "sequelize";
-interface ProductModel extends Model {
-    name: string;
-    price: number;
-    availability: boolean;
-}
-declare const Product: Sequelize.ModelCtor<ProductModel>;
-export default Product;
